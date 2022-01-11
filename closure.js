38a91682f0b963bf24ef07a0660121aa864f4bae
kozyrovladimir <kozyrovladimir@gmail.com>
2022-01-11 04:43:58 +0300

    'update closure file'

--- closure.js
+++ closure.js
@@ -5,6 +5,68 @@ const inc = function () {
    }
 }();
 
-console.log(inc());
-console.log(inc());
+// console.log(inc());
+// console.log(inc());
+
+function sum(firstNum){
+   return function (secondNum) {
+      return firstNum + secondNum;
+   }
+};
+
+//console.log(sum(1)(2));
+
+const Module = (function (){
+   let _privateValue = 1;
+   return {
+      inc: function () {
+        ++_privateValue;
+        console.log(_privateValue);
+      },
+      dec: function () {
+         --_privateValue;
+         console.log(_privateValue);
+      },
+      set: function (newValue) {
+         _privateValue = newValue;
+         console.log(_privateValue);
+      },
+      reset: function () {
+         _privateValue = 0;
+         console.log(_privateValue);
+      }
+   }
+    }
+)();
+
+// Module.inc();
+// Module.dec();
+// Module.set(8);
+// Module.reset();
+
+function toZero(num) {
+   if(num > 0) {
+      console.log(num);
+      toZero(--num);
+   } else {
+      console.log(num);
+      console.log('finish');
+   }
+}
+
+// toZero(10);
+
+// const interval = setInterval(()=> console.log('AAA') , 200);
+// setTimeout(() => clearInterval(interval) ,2000);
+
+function logA() { console.log('A') }
+function logB() { console.log('B') }
+function logC() { console.log('C') }
+function logD() { console.log('D') }
+
+// logA();
+// setTimeout(logB, 0);
+// Promise.resolve().then(logC);
+// logD();
+
 
