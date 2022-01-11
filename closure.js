10b3c5892be1b022fc32cc70075ecdfdbaa6566e
kozyrovladimir <kozyrovladimir@gmail.com>
2022-01-11 22:21:18 +0300

    'update closure file'

--- closure.js
+++ closure.js
@@ -1,41 +1,41 @@
 const inc = function () {
-   counter = 0;
-   return function () {
-      return ++counter;
-   }
+    counter = 0;
+    return function () {
+        return ++counter;
+    }
 }();
 
 // console.log(inc());
 // console.log(inc());
 
-function sum(firstNum){
-   return function (secondNum) {
-      return firstNum + secondNum;
-   }
+function sum(firstNum) {
+    return function (secondNum) {
+        return firstNum + secondNum;
+    }
 };
 
 //console.log(sum(1)(2));
 
-const Module = (function (){
-   let _privateValue = 1;
-   return {
-      inc: function () {
-        ++_privateValue;
-        console.log(_privateValue);
-      },
-      dec: function () {
-         --_privateValue;
-         console.log(_privateValue);
-      },
-      set: function (newValue) {
-         _privateValue = newValue;
-         console.log(_privateValue);
-      },
-      reset: function () {
-         _privateValue = 0;
-         console.log(_privateValue);
-      }
-   }
+const Module = (function () {
+        let _privateValue = 1;
+        return {
+            inc: function () {
+                ++_privateValue;
+                console.log(_privateValue);
+            },
+            dec: function () {
+                --_privateValue;
+                console.log(_privateValue);
+            },
+            set: function (newValue) {
+                _privateValue = newValue;
+                console.log(_privateValue);
+            },
+            reset: function () {
+                _privateValue = 0;
+                console.log(_privateValue);
+            }
+        }
     }
 )();
 
@@ -45,13 +45,13 @@ const Module = (function (){
 // Module.reset();
 
 function toZero(num) {
-   if(num > 0) {
-      console.log(num);
-      toZero(--num);
-   } else {
-      console.log(num);
-      console.log('finish');
-   }
+    if (num > 0) {
+        console.log(num);
+        toZero(--num);
+    } else {
+        console.log(num);
+        console.log('finish');
+    }
 }
 
 // toZero(10);
@@ -59,14 +59,32 @@ function toZero(num) {
 // const interval = setInterval(()=> console.log('AAA') , 200);
 // setTimeout(() => clearInterval(interval) ,2000);
 
-function logA() { console.log('A') }
-function logB() { console.log('B') }
-function logC() { console.log('C') }
-function logD() { console.log('D') }
+function logA() {
+    console.log('A')
+}
+
+function logB() {
+    console.log('B')
+}
+
+function logC() {
+    console.log('C')
+}
+
+function logD() {
+    console.log('D')
+}
 
 // logA();
 // setTimeout(logB, 0);
 // Promise.resolve().then(logC);
 // logD();
 
+const array = [1, 2, 3, 4, 5];
+
+Array.prototype.multBy = function (n) {
+    console.log(this);
+}
+
+array.multBy();
 
